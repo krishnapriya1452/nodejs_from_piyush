f11bba5fd46d6aac5ea43dde9f47adf3db22711d
krishnapriya <vkpgurram@gmail.com>
2024-06-08 21:51:14 +0530

    url parsing

--- server/index.js
+++ server/index.js
@@ -1,15 +1,24 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req,res) => {
+    if (req.url === "/favicon.ico") return res.end();
     const log = `${Date.now()}: ${req.url} New request received\n`
+    const myUrl = url.parse(req.url,true);
+    console.log(myUrl);
     fs.appendFile("log.txt",log,(err,data)=>{
-        switch(req.url){
+        switch(myUrl.pathname){
             case "/":
                 res.end("HomePage");
                 break;
             case "/about":
-                res.end("I am Krishna Priya");
+                const username = myUrl.query.myname;
+                res.end(`Hi, ${username}`);
+                break;
+            case "/search":
+                const search = myUrl.query.search_query;
+                res.end("Here are your search results " + search);
                 break;
             default:
                 res.end("404 Not found");
